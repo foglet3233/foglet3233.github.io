1d733b221d31577fb207da2e152c2833b0587c3a
foglet3233 <foglet3233@student.faytechcc.edu>
2024-12-12 15:01:29 -0500

    Create main.py

---
+++ P1LAB1/main.py
@@ -0,0 +1,8 @@
+# Tristan Fogle
+# 10/02/2024
+# P1LAB1 
+# a basic python script that takes your first and last name as inputs, then uses a print statement with an f string to welcome you to cti 110
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+# Displaying the output
+print(f"Hello, {firstName} {lastName}! Welcome to CTI-110")
